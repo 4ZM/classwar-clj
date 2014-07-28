0ba73596fc5f6d5efebaaf40a1359ef4843c2c5b
Anders Sundman <anders@4zm.org>
2014-07-28 10:18:31 +0200

    GPL licensing

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject classwar "0.1.0-SNAPSHOT"
   :main classwar.classwar
-  :description "FIXME: write description"
+  :description "Class War - a revolutionary game"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GPL - v 3.0"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lonocloud/synthread "1.0.5"]])
